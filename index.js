82e0c20c7c6cee36807e8e4b4b4be2e688152f3f
bartekkomorowski <bartekkomorowski34@gmail.com>
2020-07-17 18:25:09 +0200

    creating functionality of adding and deleting items

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-console.log("asd");
+const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todo-button");
+const todoList = document.querySelector(".todo-list");
+const todoNumber = document.querySelector(".todo-number");
+const selectType = document.querySelector(".type-product");
+const selectorValue = selectType[selectType.selectedIndex].value;
+
+todoButton.addEventListener("click", addTodo);
+todoList.addEventListener("click", deleteCheck);
+
+function addTodo(e) {
+  e.preventDefault();
+  const todoDiv = document.createElement("div");
+  todoDiv.classList.add("todo");
+
+  const newTodo = document.createElement("li");
+  newTodo.innerText = todoInput.value + " x" + todoNumber.value;
+  newTodo.classList.add("todo-item");
+  todoDiv.appendChild(newTodo);
+
+  //   const completedButton = document.createElement("button");
+  //   completedButton.innerText = "Completed";
+  //   completedButton.classList.add("complete-btn");
+  //   todoDiv.appendChild(completedButton);
+
+  const trashButton = document.createElement("button");
+  trashButton.innerText = "Trash";
+  trashButton.classList.add("trash-btn");
+  todoDiv.appendChild(trashButton);
+
+  todoList.appendChild(todoDiv);
+
+  todoInput.value = "";
+}
+
+function deleteCheck(e) {
+  const item = e.target;
+
+  if (item.classList[0] === "trash-btn") {
+    const todo = item.parentElement;
+    todo.remove();
+  }
+}
