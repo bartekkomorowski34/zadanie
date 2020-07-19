b91ef2056d1dfd9693ee31057b5e1f7211ed72bd
bartekkomorowski <bartekkomorowski34@gmail.com>
2020-07-19 11:46:34 +0200

    last commit

--- index.js
+++ index.js
@@ -1,43 +1,59 @@
-const todoInput = document.querySelector(".todo-input");
-const todoButton = document.querySelector(".todo-button");
-const todoList = document.querySelector(".todo-list");
-const todoNumber = document.querySelector(".todo-number");
-const selectType = document.querySelector(".type-product");
-const selectorValue = selectType[selectType.selectedIndex].value;
+const listInput = document.querySelector(".list-input");
+const listButton = document.querySelector(".list-button");
+const list = document.querySelector(".list");
+const listNumber = document.querySelector(".list-number");
+const selectType = document.querySelector(".list-product");
 
-todoButton.addEventListener("click", addTodo);
-todoList.addEventListener("click", deleteCheck);
+listButton.addEventListener("click", addProduct);
+list.addEventListener("click", deleteCheck);
 
-function addTodo(e) {
+function addProduct(e) {
   e.preventDefault();
-  const todoDiv = document.createElement("div");
-  todoDiv.classList.add("todo");
 
-  const newTodo = document.createElement("li");
-  newTodo.innerText = todoInput.value + " x" + todoNumber.value;
-  newTodo.classList.add("todo-item");
-  todoDiv.appendChild(newTodo);
+  const shopDiv = document.createElement("div");
+  shopDiv.classList.add("todo");
 
-  //   const completedButton = document.createElement("button");
-  //   completedButton.innerText = "Completed";
-  //   completedButton.classList.add("complete-btn");
-  //   todoDiv.appendChild(completedButton);
+  let input = document.createElement("input");
+  input.value = `${listInput.value} x${listNumber.value} ${selectType.value}`;
+  input.disabled = true;
+  input.type = "text";
+  input.classList.add("list-item");
+  shopDiv.appendChild(input);
+
+  const editButton = document.createElement("button");
+  editButton.innerText = "Edit";
+  editButton.classList.add("edit-btn");
+  shopDiv.appendChild(editButton);
+  editButton.addEventListener("click", edit);
+  function edit() {
+    input.disabled = !input.disabled;
+  }
 
   const trashButton = document.createElement("button");
-  trashButton.innerText = "Trash";
-  trashButton.classList.add("trash-btn");
-  todoDiv.appendChild(trashButton);
+  trashButton.innerText = "Delete";
+  trashButton.classList.add("delete-btn");
+  shopDiv.appendChild(trashButton);
 
-  todoList.appendChild(todoDiv);
+  list.appendChild(shopDiv);
 
-  todoInput.value = "";
+  listInput.value = "";
 }
 
 function deleteCheck(e) {
   const item = e.target;
 
-  if (item.classList[0] === "trash-btn") {
-    const todo = item.parentElement;
-    todo.remove();
+  if (item.classList[0] === "delete-btn") {
+    const list = item.parentElement;
+    list.remove();
   }
+}
+function printList() {
+  var List = document.getElementById("list-conteiner").innerHTML;
+  var content = window.open("", "", "height=500, width=500");
+  content.document.write("<html>");
+  content.document.write("<body > <h1>Div contents are <br>");
+  content.document.write(List);
+  content.document.write("</body></html>");
+  content.document.close();
+  content.print();
 }
